8455dcc855d4a907819fe4aca18c1fec8ed9ee2e
HAXAZE <28skofficial01@gmail.com>
2024-06-12 22:57:09 +0530

    completed the frontend and connected with backend

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,9 @@
+export const Quote = () => {
+    return <div className="bg-gray-300 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg text-center text-1.5xl font-bold">
+                "Sharing knowledge through blogging not only enlightens others but also enriches our own understanding. Together, let's inspire, educate, and grow with every word we write."
+            </div>
+        </div>
+    </div>
+}
